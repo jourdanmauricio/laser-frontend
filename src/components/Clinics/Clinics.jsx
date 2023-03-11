fbc420c1e5c365c624ad451cf59c44028a2c838f
mauricio <jourdanmauricio@gmail.com>
2023-03-11 11:30:39 -0300

    Fix grid clinics

--- src/components/Clinics/Clinics.jsx
+++ src/components/Clinics/Clinics.jsx
@@ -1,19 +1,20 @@
 const Clinics = ({ clinics }) => {
   return (
     <section className="py-10 px-5 lg:px-20 text-center">
-      <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+      {/* <div className="flex flex-col md:flex-row justify-center items-center gap-8"> */}
+      <div className="flex flex-wrap justify-center items-center gap-8">
         {clinics.map((clinic) => (
           <div
             key={clinic.id}
-            className="w-full md:w-1/2 py-5 px-10 border border-gray-700"
+            className="min-w-[350px] py-5 px-10 border border-gray-700 flex-grow-0 flex-shrink-0 basis-full sm:basis-1/4"
           >
             <p>
               <strong>{clinic.name}</strong>
             </p>
-            <p className="mt-2">{clinic.email}</p>
-            <p>{clinic.phone}</p>
+            <p className="mt-2 text-sm">{clinic.email}</p>
+            <p className="text-sm">{clinic.phone}</p>
 
-            <p>
+            <p className="text-sm">
               {clinic.street}, nro {clinic.number}
               {clinic.floor && (
                 <span>
@@ -22,7 +23,7 @@ const Clinics = ({ clinics }) => {
               )}
             </p>
 
-            <p>
+            <p className="text-sm">
               {clinic.state}, <strong>{clinic.city}</strong> - {clinic.cp}
             </p>
           </div>
